057bca4d04b3cb058194a66cbf338ef4acc4fb14
Steven Paul Ettrick <111305928+Steggz1989@users.noreply.github.com>
2022-09-30 17:12:32 +0100

    Add files via upload
    JavaScript update

---
+++ js/myscript.js
@@ -0,0 +1,27 @@
+var name = "Steven"
+
+let loc = "England"
+
+const dob = "5 September"
+
+console.log(name)
+console.log(loc)
+console.log(dob)
+
+let sum = 10 + 10
+console.log(sum)
+
+let num1=60, num2=60, num3= 24
+
+sum = num1 + num2
+console.log(sum)
+
+let num4="50", num5=60
+
+if(num5 == num4){
+    console.log("they are eqaul")
+}
+else {
+    console.log("they are not equal")
+}
+
